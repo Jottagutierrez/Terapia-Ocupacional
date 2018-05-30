266174bf995c11528f85519dd60c5a3240a25896
iperez-cotapos <iperez-cotapos@alumnos.uai.cl>
2018-05-30 10:34:16 -0400

    Add files via upload

---
+++ proy_prueba_30-05-2018/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+files_folder_path = 'proy_files'
+DB_path = files_folder_path + '/DB_2018.xlsx'
+param_folder_path = files_folder_path + '/parameters'
+result_folder_path = files_folder_path + '/results'
+param_path_list = {'prof_keys': param_folder_path + '/prof_keys.json',
+                   'cent_keys': param_folder_path + '/cent_keys.json',
+                   'cent_info': param_folder_path + '/cent_info.json',
+                   'week_keys': param_folder_path + '/week_keys.json',
+                   'act_keys': param_folder_path + '/act_keys.json',
+                   'Conj_U': param_folder_path + '/Conj_U.json',
+                   'Conj_A': param_folder_path + '/Conj_A.json',
+                   'Conj_Sup': param_folder_path + '/Conj_Sup.json',
+                   'Conj_Ex': param_folder_path + '/Conj_Ex.json',
+                   'Conj_Corr': param_folder_path + '/Conj_Corr.json',
+                   'Conj_E': param_folder_path + '/Conj_E.json',
+                   'Conj_B': param_folder_path + '/Conj_B.json',
+                   'Conj_P': param_folder_path + '/Conj_P.json',
+                   'Conj_S': param_folder_path + '/Conj_S.json',
+                   'Conj_Lin': param_folder_path + '/Conj_Lin.json',
+                   'T': param_folder_path + '/T.json',
+                   'D': param_folder_path + '/D.json',
+                   'C_b': param_folder_path + '/C_b.json',
+                   'C_t': param_folder_path + '/C_t.json'}
