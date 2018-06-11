fad41fb42be92832f1f36219dc441791f4b6e438
Jottagutierrez <ciguti_ronal.410@hotmail.com>
2018-06-11 11:34:44 -0400

    Add files via upload

---
+++ proy_prueba_11-06-2018/Grid.py
@@ -0,0 +1,157 @@
+import sys
+from PyQt5.QtWidgets import QSizePolicy, QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QGridLayout, QMainWindow, QLabel, QDesktopWidget, QDoubleSpinBox, QFileDialog
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, QRect, Qt
+from PyQt5 import QtGui
+import matplotlib.pyplot as plt
+import numpy as npy
+import mplcursors
+import model_solver_script as mss
+import module_export as mexp
+import db_processing_script as dbp
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+ 
+class App(QWidget):
+ 
+    def __init__(self):
+        super().__init__()
+        self.title = 'Asignación Terapia Ocupacional Universidad de los Andes'
+        self.left = 600 #cambia la posicion con respecto a la esquina superior izquierda
+        self.top = 300  #cambia la posicion con respecto a la esquina superior izquierda
+        self.width = 1000 #cambia el ancho de la ventana
+        self.height = 600 #cambia el alto de la ventana
+        self.initUI()
+ 
+    def initUI(self):
+        self.setWindowTitle(self.title) #setea el nombre de la ventana
+        self.setGeometry(self.left, self.top, self.width, self.height) #setea el tamaño de la ventana y su posición inicial
+        
+        #centra la ventana parametricamente segun la geometría de la pantalla
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center() #encuentra el centro de la pantalla
+        qr.moveCenter(cp) #hace coincidir el centro de la ventana con el de la pantalla
+        self.move(qr.topLeft())
+ 
+        #genera un grid sobre el cual se posicionaran los widgets
+        self.createGridLayout() #se llama la función createGridLyout
+        #windowLayout = QVBoxLayout()#no tengo idea
+        #windowLayout.addWidget(self.horizontalGroupBox)
+        #self.setLayout(windowLayout)#no tengo idea
+        self.showMaximized()#muestra la ventana en pantalla completa
+ 
+    def createGridLayout(self):
+        #self.horizontalGroupBox = QGroupBox("Grid")
+        layout = QGridLayout()
+        self.setLayout(layout)
+        #layout.setColumnStretch(0, 300)
+        #layout.setColumnStretch(2, 3)
+        
+        #crea los botones y los relaciona con su respectiva función
+        btn1 = QPushButton('Subir Base de Datos')
+        #btn1.resize(btn1.sizeHint())#automatiza su tamaño
+        layout.addWidget(btn1,2,0) #añade widget en el grid fila, columna
+        btn1.setMinimumSize(0,40)
+        btn1.clicked.connect(self.upload)
+        
+        btn2 = QPushButton('Exportar resultados')
+        btn2.setMinimumSize(0,40)
+        btn2.resize(btn2.sizeHint())
+        layout.addWidget(btn2,2,2) 
+        btn2.clicked.connect(self.export)
+        
+        btn3 = QPushButton('Salir')
+        btn3.setMinimumSize(0,40)
+        btn3.resize(btn3.sizeHint())
+        layout.addWidget(btn3,2,4)
+        btn3.clicked.connect(self.cancel)
+        
+        self.figure = plt.figure(figsize = (15,5)) #genera el atributo figura
+        self.canvas = FigureCanvas(self.figure) #relaciona la figura (plot) a el canvas
+        self.toolbar = NavigationToolbar(self.canvas,self) #relaciona la toolbar con el canvas
+        layout.addWidget(self.canvas, 1,1,1,3) #agrega widget (widget, fila, columna, span fila, span columna)
+        layout.addWidget(self.toolbar, 0,0,1,3)
+ 
+        #self.horizontalGroupBox.setLayout(layout)
+        
+    def upload(self):
+        options = QFileDialog.Options()
+        #options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"Seleccione Planilla de Datos", "","Hoja de Cálculo (*.xlsx)", options=options)
+        if fileName:
+            dbp.processing(fileName)
+            deltaOptions =[i/30 for i in range(3)]
+            DataFrame=mss.correr(deltaOptions)
+            self.x=DataFrame[0]
+            self.y=DataFrame[1]
+            self.deltaOptions=DataFrame[2]
+            self.graficar()
+        
+
+    def graficar(self):
+        plt.cla()
+        ax = self.figure.add_subplot(111)
+        #self.x = [i for i in range(100)]
+        #self.y = [0.95**i for i in self.x]
+        #ax.plot(self.x, self.y, '-', color='blue')
+        print('Y vale: '+ str(self.y))
+        #print('X vale: ' + str(self.x[0]))
+        ax.scatter(self.x, self.y, color='blue', picker=1)
+        
+        plt.xlabel('Costo')
+        plt.ylabel('Sobrecarga Total')
+        plt.tight_layout(rect=[0, 0, 1, 0.985])
+        ax.set_title('Curva Óptima')
+        #mplcursors.cursor(hover=False)
+        self.highlight, = ax.plot([], [], 'o', color='red')
+        self.figure.canvas.mpl_connect('pick_event', self.onPick)
+        
+        c1 = mplcursors.cursor(hover=False)
+        @c1.connect("add")
+        def _(sel):
+            sel.annotation.set(position=(100-self.ind, self.ind/2))
+            # Note: Needs to be set separately due to matplotlib/matplotlib#8956.
+            sel.annotation.arrow_patch.set(arrowstyle="simple", fc="white", alpha=.5)
+            sel.annotation.get_bbox_patch().set(fc="pink")
+        
+        self.canvas.draw()
+        #pcs.procesar()
+        
+    def onPick(self, event=None):
+        '''this_point = event.artist
+        x_value = this_point.get_xdata()
+        y_value = this_point.get_ydata()'''
+        self.ind = event.ind
+        self.highlight.set_data(npy.take(self.x, self.ind),npy.take(self.y, self.ind))
+        self.deltaExport=self.ind
+        self.canvas.draw_idle()
+        
+            
+    def export(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(self,"Guardar Archivo","","Hoja de Cálculo (*.xlsx);;Text Files (*.txt)", options=options)
+        if fileName:
+            DataFrame =mss.correr(self.deltaExport)
+            G=DataFrame[3]
+            Conj_B=DataFrame[4]#
+            Conj_U=DataFrame[5]#
+            Conj_S=DataFrame[6]#
+            Conj_Lin=DataFrame[7]
+            X=DataFrame[8]#
+            Y=DataFrame[9]#
+            W=DataFrame[10]
+            week_keys=DataFrame[11]#
+            cent_keys=DataFrame[12]
+            cent_info=DataFrame[13]
+            mexp.F_export_model_results(G, Conj_B, Conj_U, Conj_S, Conj_Lin, X, Y, W, week_keys, cent_keys, cent_info, fileName)
+            print(fileName)
+        
+    
+    def cancel(self):
+        self.close()
+    
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
---
+++ proy_prueba_11-06-2018/module_export.py
@@ -0,0 +1,233 @@
+# -*- coding: utf-8 -*-
+import xlsxwriter
+#from datetime import date
+import settings as st
+
+######################################################
+#   Función donde se crea la planilla excel con los resultados del modelo
+#   incorporando las funciones de creación de hojas...
+######################################################
+def F_student_rotative(X, Conj_B, tb, prof, cent, rot):
+    pract_info = {
+            'Practica - I': ['Supervision', 'Correccion'],
+            'Practica Profesional': ['Supervision', 'Correccion'],
+            'Internado': ['Supervision', 'Correccion', 'Examen'],
+            'Mencion': ['Supervision', 'Correccion', 'Examen']
+            }
+    est_count = []
+    for act in X[prof]:
+        if ((Conj_B[act]['Centro'] == cent) and (Conj_B[act]['Rotativa'] == rot)):
+            for pr in pract_info.keys():
+                if (pr in tb):
+                    for act_type in pract_info[pr]:
+                        if (act_type in tb):
+                            if ((Conj_B[act]['Practica'] == pr) and
+                                (Conj_B[act]['Tipo'] == act_type)):
+                                est_count.append((Conj_B[act]['Linea']))
+    est_count = len(list(set(est_count)))
+    return est_count
+
+def F_cent_student_row(X, Conj_B, tb, prof, cent_asigned):
+    cent_row = {}
+    for cent in cent_asigned:
+        cent_row[cent] = {}
+        est_sum = 0
+        for rot in range(1, 7):
+            cent_row[cent][rot] = F_student_rotative(X, Conj_B, tb, prof, cent, rot)
+            est_sum = est_sum + cent_row[cent][rot]
+        cent_row[cent]['Total'] = est_sum
+    return cent_row
+
+def F_export_model_results(G, Conj_B, Conj_U, Conj_S, Conj_Lin, X, Y, W,
+                           week_keys, cent_keys, cent_info, fileName):    
+    
+    workbook = xlsxwriter.Workbook(str(fileName))
+    
+    cell_format = {'Normal': workbook.add_format(),
+                   'Normal_Centered': workbook.add_format(),
+                   'Title_BG_Centered_Yellow': workbook.add_format(),
+                   'Title_BG_Centered_LightBlue': workbook.add_format()}
+    for f in cell_format.keys():
+        cell_format[f].set_align('vcenter')
+        cell_format[f].set_text_wrap()
+        cell_format[f].set_border(1)
+        if 'Title' in f:
+            cell_format[f].set_bold()
+        if 'Centered' in f:
+            cell_format[f].set_align('center')
+    cell_format['Title_BG_Centered_Yellow'].set_bg_color('#FFFF00')
+    cell_format['Title_BG_Centered_LightBlue'].set_bg_color('#A9D0F5')
+    
+    #   Crear la hoja con el resumen de los profesores que realizarán cada tipo
+    #   de actividad por cada centro...
+    worksheet = workbook.add_worksheet('Resumen Centros')
+    worksheet.set_column(0, 1, 30)
+    worksheet.set_column(2, 2, 15)
+    worksheet.set_column(3, 5, 20)
+    worksheet.merge_range(0, 0, 1, 0, 'Nombre CDP', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.merge_range(0, 1, 1, 1, 'Especialidad', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.merge_range(0, 2, 1, 2, 'Docente Supervisor', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.merge_range(0, 3, 0, 5, 'Docente Corrector', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.write(1, 3, '4to', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.write(1, 4, '5to', cell_format['Title_BG_Centered_Yellow'])
+    worksheet.write(1, 5, 'Examen', cell_format['Title_BG_Centered_Yellow'])
+    
+    row = 2
+    for c in cent_keys:
+        worksheet.write(row, 0, c, cell_format['Normal'])
+        c_esp = ''
+        p_sup = []
+        p_corr = {'4to': [], '5to': []}
+        p_ex = []
+        
+        for esp in cent_info[c]:
+            if 'COMUNITARIO' in esp:
+                c_esp = 'COMUNITARIO'
+            else:
+                c_esp = esp
+        for p in G.keys():
+            if c in G[p].keys():
+                p_sup.append(p)
+        for p in X.keys():
+            for act in X[p]:
+                if Conj_B[act]['Centro'] == c:
+                    if Conj_B[act]['Tipo'] == 'Correccion':
+                        if (Conj_B[act]['Practica'] == 'Practica - I' or
+                            Conj_B[act]['Practica'] == 'Practica Profesional'):
+                            p_corr['4to'].append(p)
+                        elif (Conj_B[act]['Practica'] == 'Internado' or
+                            Conj_B[act]['Practica'] == 'Mencion'):
+                            p_corr['5to'].append(p)
+                    elif Conj_B[act]['Tipo'] == 'Examen':
+                        p_ex.append(p)
+                    p_corr['4to'] = list(set(p_corr['4to']))
+                    p_corr['5to'] = list(set(p_corr['5to']))
+                    p_ex = list(set(p_ex))
+        worksheet.write(row, 1, c_esp, cell_format['Normal'])
+        worksheet.write(row, 2, ', '.join(p_sup), cell_format['Normal'])
+        worksheet.write(row, 3, ', '.join(p_corr['4to']), cell_format['Normal'])
+        worksheet.write(row, 4, ', '.join(p_corr['5to']), cell_format['Normal'])
+        worksheet.write(row, 5, ', '.join(p_ex), cell_format['Normal'])
+        
+        row+=1
+    ######################################################
+    
+    #   Crear la hoja con la tabla resumen con las horas de excedente para cada
+    #   profesor en cada semana...
+    worksheet = workbook.add_worksheet('Excedente (hrs)')
+    worksheet.write(0, 0, 'Semana')
+    col=1
+    for p in X.keys():
+        if p != 'Externalizaciones':
+            for s in range(0, max(week_keys)+1):
+                try:
+                    worksheet.write(s+1, col, Y[p][s])
+                    '''
+                    if (Y[p][s] > 0):
+                        worksheet.write(s+1, col, Y[p][s])
+                    elif (W[p][s] > 0):
+                        worksheet.write(s+1, col, (-1 * W[p][s]))
+                        '''
+                except KeyError:
+                    #worksheet.write(s+1, col, (-1 * W[p][s]))
+                    pass
+                worksheet.write(s+1,0, s+1)
+            worksheet.write(0, col, p)
+            col+=1
+            
+    worksheet = workbook.add_worksheet('Subcarga (hrs)')
+    worksheet.write(0, 0, 'Semana')
+    col=1
+    for p in X.keys():
+        if p != 'Externalizaciones':
+            for s in range(0, max(week_keys)+1):
+                try:
+                    worksheet.write(s+1, col, W[p][s])
+                except KeyError:
+                    pass
+                worksheet.write(s+1,0, s+1)
+            worksheet.write(0, col, p)
+            col+=1
+    ######################################################
+    
+    #   Crear las hojas con la asignación de actividades para cada profesor...
+    for p in X.keys():
+        worksheet = workbook.add_worksheet(p)
+        
+        worksheet.write(0, 0, 'Semana')
+        worksheet.write(0, 1, 'Actividad')
+        worksheet.write(0, 2, 'Tipo')
+        worksheet.write(0, 3, 'Especialidad')
+        worksheet.write(0, 4, 'Centro')
+        worksheet.write(0, 5, 'Practica')
+        worksheet.write(0, 6, 'Rotativa')
+        
+        row=1
+        for s in Conj_U.keys(): #en todas las semanas posibles
+            for k in Conj_U[s]: #en todas las actividades de esa semana
+                for act in X[p]:#todas las atividades realizados por el profesor
+                    if k == int(act): #si la actividad realizada corresponde a las actividades realizadas esa semana
+                        worksheet.write(row, 0, (int(s)+1))
+                        worksheet.write(row, 1, act)
+                        worksheet.write(row, 2,Conj_B[k]['Tipo'])
+                        worksheet.write(row, 3,', '.join(Conj_B[k]['Especialidad']))
+                        worksheet.write(row, 4,Conj_B[k]['Centro'])
+                        worksheet.write(row, 5,Conj_B[k]['Practica'])
+                        worksheet.write(row, 6,Conj_B[k]['Rotativa'])
+                        row += 1
+        
+        row = 21
+        
+        rot_info = {'Practica - I': 6, 'Practica Profesional': 6, 'Internado': 4, 'Mencion': 1}
+        
+        table_list = ['Practica - I (Supervision)', 'Practica - I (Correccion)',
+                      'Practica Profesional (Supervision)', 'Practica Profesional (Correccion)',
+                      'Internado (Supervision)', 'Internado (Correccion)',
+                      'Internado (Examen)',
+                      'Mencion (Supervision)', 'Mencion (Correccion)',
+                      'Mencion (Examen)']
+        
+        cent_asigned = []
+        for act in X[p]:
+            cent_asigned.append(Conj_B[act]['Centro'])
+        cent_asigned = list(set(cent_asigned))
+        
+        worksheet.set_column(10, 10, 40)
+        for tb in table_list:
+            col = 10
+            
+            cent_row = F_cent_student_row(X, Conj_B, tb, p, cent_asigned)
+            total_tb = 0
+            for cent in cent_row.keys():
+                total_tb = total_tb + cent_row[cent]['Total']
+            
+            for pr in rot_info.keys():
+                if ((total_tb > 0) and (pr in tb)):
+                    worksheet.write(row, col, tb, cell_format['Title_BG_Centered_LightBlue'])
+                    worksheet.write(row+1, col, 'Centros/Rotativa', cell_format['Title_BG_Centered_LightBlue'])
+                    worksheet.merge_range(row, col+1, row, col+rot_info[pr], 'Alumnos por Rotativa', cell_format['Title_BG_Centered_LightBlue'])
+                    for i in range(1, rot_info[pr]+1):
+                        worksheet.write(row+1, col+i, i, cell_format['Title_BG_Centered_LightBlue'])
+                    worksheet.merge_range(row, col+rot_info[pr]+1, row+1, col+rot_info[pr]+1, 'Total por Centro', cell_format['Title_BG_Centered_LightBlue'])
+                    worksheet.merge_range(row, col+rot_info[pr]+2, row+1, col+rot_info[pr]+2, 'Total', cell_format['Title_BG_Centered_LightBlue'])
+                    row+=2
+                    
+                    row_i = row
+                    
+                    cent_count = 0
+                    for cent in cent_row.keys():
+                        if (cent_row[cent]['Total'] > 0):
+                            cent_count+=1
+                            worksheet.write(row, col, cent, cell_format['Normal'])
+                            for rot in range(1, rot_info[pr]+1):
+                                worksheet.write(row, col+rot, cent_row[cent][rot], cell_format['Normal'])
+                            worksheet.write(row, col+rot_info[pr]+1, cent_row[cent]['Total'], cell_format['Normal'])
+                            row+=1
+                    if (cent_count > 1):
+                        worksheet.merge_range(row_i, col+rot_info[pr]+2, row-1, col+rot_info[pr]+2, total_tb, cell_format['Normal_Centered'])
+                    else:
+                        worksheet.write(row-i, col+rot_info[pr]+2, total_tb, cell_format['Normal_Centered'])
+                    row+=1
+    ######################################################
+    
+    workbook.close()
